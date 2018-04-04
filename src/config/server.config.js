353d6d389533210ee22fcecc6715bc640b75670a
Cphayim <admin@cphayim.me>
2018-04-04 09:47:16 +0800

    init(config): 服务端配置文件

---
+++ src/config/server.config.js
@@ -0,0 +1,11 @@
+/* 
+ * 服务器相关配置项
+ * @Author: Cphayim 
+ * @Date: 2018-04-04 01:12:06 
+ * @Last Modified by: Cphayim 
+ * @Last Modified time: 2018-04-04 01:12:06 
+ */
+
+export const server = {
+
+}
