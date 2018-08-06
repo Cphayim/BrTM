e942bbc11f48f95a65aaf7b7f8e42e456ea720fa
Cphayim <admin@cphayim.me>
2018-08-06 23:14:49 +0800

    feat: 入口以及公用样式配置和混合宏

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,44 @@
-/******************************************************************
-MIT License http://www.opensource.org/licenses/mit-license.php
-Author Mora <qiuzhongleiabc@126.com> (https://github.com/qiu8310)
-*******************************************************************/
+/**
+ * Created by Cphayim at 2018-07-25 19:31
+ * => /src/app.ts
+ */
 
-import { appify, MyApp, MyStore } from 'base/'
-@appify(new MyStore(), {
+import { appify, MyApp, wxp } from 'base'
+import config from 'config'
+import store from 'base/MyStore'
+
+@appify(store, {
   pages: require('./app.cjson?pages'),
   tabBarList: require('./app.cjson?tabBar.list')
 })
 export default class extends MyApp {
-  async onLaunch() {}
+  onLaunch() {
+    this.init()
+  }
+
+  init() {
+    this.envPrepare()
+  }
+
+  /**
+   * 环境准备
+   */
+  envPrepare() {
+    const info = wxp.getSystemInfoSync()
+    this.logger(`当前正以 '${config.CLIENT.MODE}' 模式启动`)
+      .logger(`appid：${config.CLIENT.APPID}`)
+      .logger(`版本：${config.CLIENT.VERSION}`)
+      .logger(`运行平台：${info.platform}`)
+      .logger(`微信版本：${info.version}`)
+      .logger(`基础库版本：${info.SDKVersion}`)
+
+    if (config.CLIENT.MODE === 'development' && config.CLIENT.CLEAN) {
+      wxp.clearStorageSync()
+    }
+  }
+
+  logger(msg: string) {
+    console.log(`%c${msg}`, 'color:#6699cc')
+    return this
+  }
 }
